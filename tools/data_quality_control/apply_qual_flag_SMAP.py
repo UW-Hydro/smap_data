31947111dd18d3a63ae8ee69c8bdcae5738a017c
Y. Mao <yixinmao@ikt1.hyak.local>
2018-10-14 14:57:30 -0700

    Add qualtiy control scripts and README

---
+++ tools/data_quality_control/apply_qual_flag_SMAP.py
@@ -0,0 +1,77 @@
+
+# This script:
+#   1) Exclude SMAP pixels with unrecommended retrieval quality flag (according to SMAP internal flag)
+
+import sys
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+import pandas as pd
+import os
+from joblib import Parallel, delayed, parallel_backend
+import timeit
+
+from utils import setup_output_dirs
+
+
+# ========================================== #
+# Parameter setting
+# ========================================== #
+# Input SMAP nc
+smap_nc = '/civil/hydro/ymao/smap_data/tools/data_quality_control/output/data/smap.PM_rescaled.20150331_20171231.nc'
+# SMAP quality control flag nc
+smap_qual_flag_nc = '/civil/hydro/ymao/smap_data/tools/prepare_SMAP/output/data/soil_moisture.20150331_20171231.nc'
+# SMAP domain nc
+domain_nc = '/civil/hydro/ymao/smap_data/param/domain/smap.domain.global.nc'
+
+# Output basedir
+output_basedir = '/civil/hydro/ymao/smap_data/tools/data_quality_control/output'
+
+
+# ========================================== #
+# Load original SMAP data
+# ========================================== #
+# Load SMAP
+ds_smap = xr.open_dataset(smap_nc)
+ds_smap.load()
+ds_smap.close()
+da_smap = ds_smap['soil_moisture']
+# Load SMAP quality control flag
+ds_smap_qual_flag = xr.open_dataset(smap_qual_flag_nc)
+ds_smap_qual_flag.load()
+ds_smap_qual_flag.close()
+da_smap_qual_flag = ds_smap_qual_flag['retrieval_qual_flag']
+# Load domain
+da_domain = xr.open_dataset(domain_nc)['mask']
+# Extract dates - for output naming purpose
+start_time = pd.to_datetime(da_smap['time'].values[0])
+end_time = pd.to_datetime(da_smap['time'].values[-1])
+
+
+# ========================================== #
+# Set up directories
+# ========================================== #
+output_dir = setup_output_dirs(output_basedir, mkdirs=['data'])['data']
+
+
+# ========================================== #
+# Apply retrieval flag
+# ========================================== #
+print('Applying retrieval falg...')
+da_smap_recom_qual = da_smap.where(da_smap_qual_flag==0)
+
+
+# ========================================== #
+# Save to file
+# ========================================== #
+# --- Save new SMAP data to file --- #
+print('Save to file')
+ds_smap_new = xr.Dataset({'soil_moisture': da_smap_recom_qual})
+ds_smap_new.to_netcdf(
+    os.path.join(output_dir,
+                 'smap.PM_rescaled.recom_qual.{}_{}.nc'.format(
+                    start_time.strftime('%Y%m%d'),
+                    end_time.strftime('%Y%m%d'))),
+    format='NETCDF4_CLASSIC')
+
---
+++ tools/data_quality_control/quality_control_GPM.py
@@ -0,0 +1,71 @@
+
+# This script applies quality control for GPM IMERG data
+
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+import pandas as pd
+import os
+import timeit
+
+from utils import setup_output_dirs
+
+
+# ========================================== #
+# Parameter setting
+# ========================================== #
+# Original IMERG nc
+gpm_nc = '/civil/hydro/ymao/smap_data/tools/prepare_GPM/output/L3_Final_global.12hour.36km/prec.20150101_20171231.nc'
+# SMAP domain nc
+domain_nc = '/civil/hydro/ymao/smap_data/param/domain/smap.domain.global.nc'
+
+# Output basedir
+output_basedir = '/civil/hydro/ymao/smap_data/tools/data_quality_control/output'
+
+
+# ========================================== #
+# Set up directories
+# ========================================== #
+output_dir = setup_output_dirs(output_basedir, mkdirs=['data'])['data']
+
+
+# ========================================== #
+# Load original GPM data
+# ========================================== #
+print('Load data...')
+# Load GPM
+ds_gpm = xr.open_dataset(gpm_nc)
+da_gpm = ds_gpm['PREC']
+da_gpm.load()
+da_gpm.close()
+# Load domain
+da_domain = xr.open_dataset(domain_nc)['mask']
+# Extract dates - for output naming purpose
+start_time = pd.to_datetime(da_gpm['time'].values[0])
+end_time = pd.to_datetime(da_gpm['time'].values[-1])
+
+
+# ========================================== #
+# Exclude all-zero precipitation pixels
+# ========================================== #
+print('Exclude all-zero precipitation pixels...')
+da_prec_max = da_gpm.max(dim='time')
+
+prec = da_gpm.values
+prec[:, da_prec_max<10e-4] = np.nan
+da_gpm[:] = prec
+
+
+# ========================================== #
+# Save to file
+# ========================================== #
+# --- Save new GPM data to file --- #
+print('Save to file...')
+ds_gpm_new = xr.Dataset({'PREC': da_gpm})
+ds_gpm_new.to_netcdf(
+    os.path.join(output_dir,
+                 'prec.qc.{}_{}.nc'.format(
+                    start_time.strftime('%Y%m%d'),
+                    end_time.strftime('%Y%m%d'))),
+    format='NETCDF4_CLASSIC')
